ccb15360bd049f0d091af3935ba4af6bac988942
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-07-17 18:43:58 +0600

    1st

--- app/src/main/java/com/example/manipulatedimagedetector/MainActivity.java
+++ app/src/main/java/com/example/manipulatedimagedetector/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.manipulatedimagedetector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.service.autofill.ImageTransformation;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageView;
+    TextView textView;
+    Button loadButton, detectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        loadButton = findViewById(R.id.button_loadImage);
+        detectButton = findViewById(R.id.button_Detect);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
+
+
+    public void load(View v){
+        Image im;
+        Bitmap b = null;
+        Intent i = new Intent(
+                Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
+    public void detect(View view){
+
+    }
+
+
 }
